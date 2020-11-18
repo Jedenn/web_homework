5aacd34c2301edd57f6a024952ae436aad4529ab
djwang <wangdejie1996@gmail.com>
2020-11-18 22:16:41 +0800

    初始化0017作业

--- 0000_JavaScript设计模式/000_test.js
+++ 0000_JavaScript设计模式/000_test.js
@@ -1,17 +1,1 @@
-const fs = require("fs");
-
-fs.writeFile("1.txt", "我要写入的文字", {flag:"w"}, function(err){
-    if(err){
-        return console.log(err);
-    }else{
-        console.log("写入成功");
-    }
-})
-
-fs.readFile("1.txt", "utf8", (err,data)=>{
-    if(err){
-        return console.log(err);
-    }else{
-        console.log(data);
-    }
-})
+console.log(__dirname);
