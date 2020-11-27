7fa1788b89d9da03c52a0e3adba136145a2327f5
djwang <wangdejie1996@gmail.com>
2020-11-28 04:14:52 +0800

    1126作业

--- 1124_王德杰_前后端交互01/server/app2/app.js
+++ 1124_王德杰_前后端交互01/server/app2/app.js
@@ -1,77 +1,138 @@
-const Koa = require('koa');
-const KoaRouter = require('koa-router');
-const KoaStaticCache = require('koa-static-cache');
-const KoaBody = require('koa-body');
-const mysql2 = require('mysql2');
-const parsePath = require('parse-filepath')
+const Koa = require("koa");
+const KoaRouter = require("koa-router");
+const KoaStaticCache = require("koa-static-cache");
+const KoaBody = require("koa-body");
+const mysql2 = require("mysql2");
+const parsePath = require("parse-filepath");
+const jwt = require("koa-jwt");
+const jsonwebtoken = require("jsonwebtoken");
 
 // 创建数据库链接
 const connection = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345678',
-    database: 'kkb'
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "kkb",
 });
 
 function query(sql, prePared) {
-    return new Promise((resolve, reject) => {
-        connection.query(
-            sql,
-            prePared,
-            function(err, results, fields) {
-                if (err) {
-                    reject(err); 
-                } else {
-                    resolve([results, fields]);
-                }
-            }
-        ) 
+  return new Promise((resolve, reject) => {
+    connection.query(sql, prePared, function (err, results, fields) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve([results, fields]);
+      }
     });
+  });
 }
-
 
 const app = new Koa();
 
-app.use( KoaStaticCache('./public', {
-    prefix: '/public',
+const secret = "kkb";
+
+// 1. public目录下静态资源访问不需要jwt验证
+// 2. login接口不需要jwt验证
+// 3. static目录下静态资源访问不需要jwt验证
+app.use(jwt({ secret }).unless({ path: [/^\/public/, /^\/login/, /^\/static/] })); 
+
+app.use(
+  KoaStaticCache("./public", {
+    prefix: "/public",
     gzip: true,
-    dynamic: true
-}) );
+    dynamic: true,
+  })
+);
 
-
-// 静态资源文件代理static/upload路径下文件
-app.use( KoaStaticCache('./static/upload', {
-    prefix: '/static/upload',
+app.use(
+  KoaStaticCache("./static", {
+    prefix: "/static",
     gzip: true,
-    dynamic: true
-}) );
-
+    dynamic: true,
+  })
+);
 
 const router = new KoaRouter();
 
-
-router.post('/save', KoaBody({
-    multipart: true,
-    formidable: {
-        uploadDir: './static/upload',  // 文件保存到static/upload目录下
-        keepExtensions: true
-    }
-}), async ctx => {
-    let attachment = ctx.request.files.attachment;
-    let {base: filename} = parsePath(attachment.path);
-    let {type, size} = attachment
-    // 1. 保存文件信息到表photos中
-    let [rs] = await query('insert into `photos`(`filename`,`type`,`size`) value(?,?,?)',[filename, type, size])
-    // 2. 把上传成功后的当前这个图片的访问地址返回给 前端
-    ctx.body = "../static/upload/" + filename;
+router.post("/login", KoaBody(), async (ctx) => {
+  let { username, password } = ctx.request.body;
+  // 检查用户名密码是否在用户数据库中存在
+  let [rs] = await query(
+    `select * from users where username='${username}' and password='${password}'`
+  );
+  if (rs.length == 1) {
+    // 如果存在，则登录成功，将用户信息使用jwt加密后得到token，token被放置在响应报文头Authorization中
+    let userInfo = {
+      id: rs[0].id,
+      username: rs[0].username,
+    };
+    ctx.set("Authorization", "Bearer " + jsonwebtoken.sign(userInfo, secret)); // 设置响应报文头，格式为[Bearer ***]
+    ctx.body = "登录成功";
+  } else {
+    ctx.body = "登录失败";
+  }
 });
 
-router.get('/getPhotos', async ctx => {
-    // 从数据库中读取所有上传的图片记录数据，并返回给前端，前端通过ajax调用该接口
-    let [rs] = await query('select filename from `photos`');
-    ctx.set("content-type", "application/json");
-    ctx.body = JSON.stringify(rs);
-})
+router.post(
+  "/save",
+  KoaBody({
+    multipart: true,
+    formidable: {
+      uploadDir: "./static/upload", // 文件保存到static/upload目录下
+      keepExtensions: true,
+    },
+  }),
+  async (ctx) => {
+    let authorizationData = ctx.get("Authorization");
+    if (authorizationData) {
+      let token = authorizationData.split(" ")[1]; // authorization格式为[Bearer ***],所以要split
+      let userInfo = jsonwebtoken.verify(token, secret);  // 验证失败会抛出异常
+      console.log(userInfo.id);
+      console.log(userInfo);
+      if (userInfo.id) {
+          // 保存照片信息及其所有者用户Id
+        let attachment = ctx.request.files.attachment;
+        let { base: filename } = parsePath(attachment.path);
+        let { type, size } = attachment;
+        let [
+          rs,
+        ] = await query(
+          "insert into `photos`(`filename`,`type`,`size`, `uid`) value(?,?,?,?)",
+          [filename, type, size, userInfo.id]
+        );
+        ctx.body = filename;
+      } else {
+        ctx.body = "您尚未登录，无法上传文件";
+      }
+    }else{
+        ctx.body = "您尚未登录，无法上传文件";
+    }
+  }
+);
+
+router.get("/getPhotos", async (ctx) => {
+  // 判断用户是否有权限（是否登录）
+  let authorizationData = ctx.get("Authorization");
+  console.log(authorizationData);
+  if (authorizationData) {
+    let token = authorizationData.split(" ")[1]; // 报文头的格式是[Bearer ***]
+    let userInfo = jsonwebtoken.verify(token, secret); // 验证不通过会抛出异常
+    console.log(userInfo.id);
+    console.log(userInfo);
+    if (userInfo.id) {
+      // 获取当前用户所上传的文件
+      let [rs] = await query(
+        `select filename from photos where uid='${userInfo.id}'`
+      );
+      ctx.set("content-type", "application/json");
+      ctx.body = JSON.stringify(rs);
+    } else {
+      ctx.body = ""; // 无权访问则返回空
+    }
+  } else {
+    ctx.body = ""; // 无权返回空
+  }
+});
 
 app.use(router.routes());
 
--- 1124_王德杰_前后端交互01/server/app2/public/js/index.js
+++ 1124_王德杰_前后端交互01/server/app2/public/js/index.js
@@ -1,95 +1,124 @@
-let btnElement = document.querySelector('.btn');
-let attachmentElement = document.querySelector('#attachment');
-let taskBodyElement = document.querySelector('.task_body');
+window.onload = function () {
+  registerLoginEvent();
+  registerSaveEvent(); // 注册上传图片事件
 
+};
 
+/**
+ * 登录按钮事件：使用ajax发送登录请求
+ */
+function registerLoginEvent() {
+  let loginBtnElement = document.querySelector("#loginBtn");
+  let authorizationData = "";
 
+  loginBtnElement.onclick = function () {
+    let xhr = new XMLHttpRequest();
+    xhr.open("post", "/login", true);
 
-btnElement.onclick = function() {
-    attachmentElement.click();
+    xhr.onload = function () {
+      // 如果登录成功，与后端约定好，在报文头Authorization中返回jwt token
+      // 前端获取到token后，保存到localStorage，以便下一次请求时携带发送token到后端
+      authorizationData = xhr.getResponseHeader("Authorization");
+      if (authorizationData) {
+          alert("登录成功");
+        localStorage.setItem("authorizationData", authorizationData);
+        // 发送ajax请求获取当前用户上传的照片
+        getPhotoData();
+      }
+    };
+
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.send(
+      JSON.stringify({
+        username: document.querySelector("#username").value,
+        password: document.querySelector("#password").value,
+      })
+    );
+  };
 }
 
-attachmentElement.onchange = function() {
+/**
+ * 注册上传保存按钮事件
+ */
+function registerSaveEvent() {
+  let btnElement = document.querySelector(".btn");
+  let attachmentElement = document.querySelector("#attachment");
+  let taskBodyElement = document.querySelector(".task_body");
 
+  btnElement.onclick = function () {
+    attachmentElement.click();
+  };
+
+  attachmentElement.onchange = function () {
     const xhr = new XMLHttpRequest();
-    xhr.open('post', '/save', true);
-    
+    xhr.open("post", "/save", true);
+
     let fd = new FormData();
 
-    // xhr 下面的事件对应的是请求过程中的相关事件，请求：下载
-    // xhr.onprogress = function() {
-        // 客户端发送这次请求后，从服务器获取数据的进度过程
-    // }
+    let liElement = document.createElement("li");
+    let spanElement = document.createElement("span");
+    let taskProgressStatusElement = document.createElement("span");
+    let progressElement = document.createElement("span");
 
+    xhr.upload.onloadstart = function () {
+      taskProgressStatusElement.classList.add("task-progress-status");
+      taskProgressStatusElement.innerHTML = "开始上传……";
+      progressElement.classList.add("progress");
+      progressElement.style.width = "0%";
+      liElement.appendChild(taskProgressStatusElement);
+      liElement.appendChild(progressElement);
+      taskBodyElement.appendChild(liElement);
+      document.querySelector(".task_panel").style.display = "";
+    };
 
-    // 如果想监控上传的进度事件 upload
-    // console.log(xhr.upload);
+    xhr.upload.onprogress = function (e) {
+      let v = ((e.loaded / e.total) * 100).toFixed(2);
+      taskProgressStatusElement.innerHTML = v + "%";
+      progressElement.style.width = v + "%";
+    };
 
-    let liElement = document.createElement('li');
-    let spanElement = document.createElement('span');
-    let taskProgressStatusElement = document.createElement('span');
-    let progressElement = document.createElement('span');
+    xhr.upload.onloadend = function (e) {
+      document.querySelector(".task_panel").style.display = "none";
+    };
 
-    xhr.upload.onloadstart = function() {
-        taskProgressStatusElement.classList.add('task-progress-status');
-        taskProgressStatusElement.innerHTML = '开始上传……';
-        progressElement.classList.add('progress');
-        progressElement.style.width = '0%';
-        liElement.appendChild(taskProgressStatusElement);
-        liElement.appendChild(progressElement);
-        taskBodyElement.appendChild(liElement);
-        document.querySelector(".task_panel").style.display = "";
+    xhr.onload = function () {
+      let contentList = document.querySelector(".content-list");
+      let image = new Image();
+      image.src = "/static/upload/" + xhr.responseText;
+      contentList.appendChild(image);
+    };
+
+    // 请求头中要发送token, token中携带的个人信息用于标记所上传照片的所有者
+    let auth = localStorage.getItem("authorizationData");
+    if (auth) {
+      xhr.setRequestHeader("Authorization",   auth);
     }
-
-    xhr.upload.onprogress = function(e) {
-        // e.total : 上传的总大小 
-        // e.loaded : 已经上传的数据大小
-        let v = (e.loaded / e.total * 100).toFixed(2);
-        taskProgressStatusElement.innerHTML = v + '%';
-        progressElement.style.width = v + '%';
-    }
-
-    xhr.upload.onloadend = function(e){
-        // 上传结束后,关闭上传窗口
-        console.log("上传完毕");
-        document.querySelector(".task_panel").style.display = "none";
-    }
-
-    xhr.onload = function() {
-        //  <div class="content-list">
-        // 	<!-- 在这里显示上传的图片 -->
-        // </div>
-        let contentList = document.querySelector(".content-list");
-        let imgEle = document.createElement("img");
-        imgEle.src = '../' + xhr.responseText;  // index.html与上传文件不在同一级目录下
-        imgEle.height = 150;
-        contentList.appendChild(imgEle);
-    }
-
-    fd.append('attachment', attachmentElement.files[0]);
+    fd.append("attachment", attachmentElement.files[0]);
     xhr.send(fd);
-
+  };
 }
 
-// 当进入页面的时候，首先会调用 /getPhotos 接口从后端拿去到所有已经上传的图片数据，并显示在 content-list 中
+/**
+ * 获取当前用户所上传的图片
+ */
+function getPhotoData() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("get", "/getPhotos", true);
 
-window.onload = function(){
-    // 1. 获取上传的图片
-    let xhr = new XMLHttpRequest();
-    xhr.open("get", "/getPhotos",true);
-
-    xhr.onload = function(){
-        let photos = JSON.parse(xhr.responseText);
-        for(let photo of photos){
-            let imgSrc = "../static/upload/"+photo.filename;
-
-            let contentList = document.querySelector(".content-list");
-            let imgEle = document.createElement("img");
-            imgEle.src = imgSrc; 
-            imgEle.height = 150;
-            contentList.appendChild(imgEle)
-        }
+  xhr.onload = function () {
+    let photos = JSON.parse(xhr.responseText);
+    for (let photo of photos) {
+      let contentList = document.querySelector(".content-list");
+      let image = new Image();
+      image.src = "/static/upload/" + photo.filename;
+      contentList.appendChild(image);
     }
+  };
 
-    xhr.send();
+  // 请求头中要发送token, token中携带的个人信息可用于查询用户上传的照片
+  let auth = localStorage.getItem("authorizationData");
+  if (auth) {
+    xhr.setRequestHeader("Authorization",  auth);
+  }
+  xhr.send();
 }
